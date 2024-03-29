2bf63dccc33bb878fc11c199f909148a419a8ec0
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2024-03-29 11:21:44 -0400

    Derive clone on MessageTypes

--- src/message/model.rs
+++ src/message/model.rs
@@ -1,19 +1,7 @@
 use serde::{Deserialize, Serialize};
 use std::str::FromStr;
 
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(untagged)]
-pub enum MessageType {
-    MessageSent {},
-    RetrieveMessages,
-    RoomJoin,
-    RoomLeave,
-    UserJoin,
-    UserLeave,
-    ChangeUserData // Implement this later!
-}
-
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(untagged)]
 enum MessageTypes {
   MessageSent(MessageSent),
@@ -28,19 +16,4 @@ struct MessageSent {
 #[derive(Serialize, Deserialize, Debug)]
 struct RetrieveMessages {
     msgs: VecString
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct MessageModel {
-    // Message typing so the client and server know what they should do with results
-    #[serde(rename="type")]
-    pub msgtype: MessageType,
-
-    // Make params with aliases for each message type
-    #[serde(skip_serializing_if = "Option::is_none")]
-    #[serde(alias="msg")]
-    pub param1: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    #[serde(alias="user")]
-    pub param2: Option<String>,
 }
