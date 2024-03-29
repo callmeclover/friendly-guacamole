5e9b6c68ef4af92ace57d33e593e0855ff98fc4f
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2024-03-29 14:34:33 -0400

    debug

--- src/message/func.rs
+++ src/message/func.rs
@@ -40,7 +40,8 @@ pub fn into_censored_md(html: &str, user: &mut User) -> Result<String, BlockReas
 
     let mut index = 0;
     let mut new_text: Vec<String> = vec![];
-    while index < nodes_text.len()-1 {
+    while index < nodes_text.len() {
+        println!("{}, {}", nodes_text.len(), nodes_char.len());
         let replacement: String = nodes_char.chunks_exact(nodes_text[index].len()).next().unwrap().iter().collect();
         new_text.push(replacement);
         nodes_char.drain(0..nodes_text[index].len());
