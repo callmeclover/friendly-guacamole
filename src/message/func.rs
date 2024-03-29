9f69b3cf1a77418771f2e086db41b5efbe962e8d
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2024-03-29 15:05:26 -0400

    fix

--- src/message/func.rs
+++ src/message/func.rs
@@ -26,7 +26,7 @@ pub fn into_censored_md(html: &str, user: &mut User) -> Result<String, BlockReas
         document = kuchikiki::parse_html().one(format!("<p>{}</p>", document.select_first("body").unwrap().as_node().to_string()));
     }
 
-    let mut nodes_text: Vec<String> = document.select_first("p").expect("").descendants().text_nodes().map(|text| {<RefCell<String> as Clone>::clone(&text).into_inner()}).collect();
+    let mut nodes_text: Vec<String> = document.select_first("p").expect("").as_node().descendants().text_nodes().map(|text| {<RefCell<String> as Clone>::clone(&text).into_inner()}).collect();
     nodes_text.pop();
     let mut nodes_char: Vec<char> = user.context.process(nodes_text.join("").trim().to_string())?.chars().collect();
 
@@ -38,7 +38,7 @@ pub fn into_censored_md(html: &str, user: &mut User) -> Result<String, BlockReas
         nodes_char.drain(0..text.len());
     }
 
-    for (index, text_node) in document.select_first("p").expect("").descendants().text_nodes().enumerate() {
+    for (index, text_node) in document.select_first("p").expect("").as_node().descendants().text_nodes().enumerate() {
         text_node.replace(new_text[index].clone());
     }
     if document.descendants().text_nodes().map(|text| {<RefCell<String> as Clone>::clone(&text).into_inner()}).collect::<Vec<String>>().join("").trim().is_empty() {
