83cdbcf1353b1b55a4110ef8b8def379f1e5a35c
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2024-03-29 11:28:38 -0400

    fix 7

--- src/main.rs
+++ src/main.rs
@@ -111,7 +111,7 @@ async fn handle_socket(socket: WebSocket, _who: SocketAddr, state: Arc<AppState>
     *USER_ID.lock().unwrap() += 1;
     let username = USER_ID.lock().unwrap().clone().to_string();
 
-    sender.send(Message::Text(format!("\"msgs\": {}", serde_json::to_string(&MESSAGES.lock().unwrap()).expect("couldn't serialize MESSAGES vector!"))));
+    sender.send(Message::Text(format!("\"msgs\": {}", serde_json::to_string(&*MESSAGES.lock().unwrap()).expect("couldn't serialize MESSAGES vector!"))));
 
     // We subscribe *before* sending the "joined" message, so that we will also
     // display it to our client.
