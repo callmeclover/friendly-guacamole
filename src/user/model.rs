3bcba83b2c7a23d228dfc13465c23dfec68e318d
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2024-04-23 13:06:59 -0400

    fix 4

--- src/user/model.rs
+++ src/user/model.rs
@@ -42,6 +42,7 @@ impl GlassModeration {
     /// If the user is muted, it returns an error.
     pub fn process(&self, input: &str) -> Result<&str, Box<dyn Error>> {
         if self.is_muted { return Err("User is muted".into()); }
+        
         let (censored, analysis) = Censor::from_str(input)
             .with_censor_threshold(Type::SEVERE)
             .with_censor_first_character_threshold(Type::OFFENSIVE & Type::SEVERE)
@@ -49,6 +50,7 @@ impl GlassModeration {
             .with_ignore_self_censoring(false)
             .with_censor_replacement('*')
             .censor_and_analyze();
+
         if analysis.is(Type::OFFENSIVE & Type::SEVERE) {
             self.warn();
             Err("Message is inappropriate".into())
@@ -61,7 +63,7 @@ impl GlassModeration {
     }
 
     /// Warns the user, adding a report if there are 5 warnings.
-    pub fn warn(&self) {
+    pub fn warn(&mut self) {
         self.warnings += 1;
         if self.warnings >= 5 {
             self.warnings = 0;
@@ -70,7 +72,7 @@ impl GlassModeration {
     }
 
     /// Reports the user, muting them if there are 10 warnings.
-    pub fn report(&self) {
+    pub fn report(&mut self) {
         self.reports += 1;
         if self.reports >= 10 {
             self.is_muted = true;
@@ -78,8 +80,8 @@ impl GlassModeration {
     }
 
     /// Mutes the user.
-    pub fn mute(&self) { self.is_muted = true; }
+    pub fn mute(&mut self) { self.is_muted = true; }
 
     /// Unmutes the user.
-    pub fn unmute(&self) { self.is_muted = false; }
+    pub fn unmute(&mut self) { self.is_muted = false; }
 }
